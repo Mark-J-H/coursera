e9b5150b323a0c4be4fdd58ee067366fa8cc0225
Mark-J-H <markjameshinton@gmail.com>
2024-01-25 22:39:35 -0500

    list items

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import SvgComponent from './header';
+import Navigation from './nav';
 
 function App() {
   return (
     <>
+    <Navigation/>
     <SvgComponent/>
     </>
   );
--- myapp/src/nav.js
+++ myapp/src/nav.js
@@ -1,14 +1,16 @@
-export function nav() {
+function Navigation() {
     return (
-      <>
+      <nav>
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li><a href="/Home">Homepage</a></li>
+        <li><a href="/About">About</a></li>
+        <li><a href="/Menu">Menu</a></li>
+        <li><a href="/Reservations">Reservations</a></li>
+        <li><a href="/Order Online">Order Online</a></li>
+        <li><a href="/Login">Login</a></li>
       </ul>
-      </>
+      </nav>
     );
   }
+
+  export default Navigation
