bbe39abd45d43e3207fb76fcdc189dd012760194
Mark-J-H <markjameshinton@gmail.com>
2024-02-28 21:51:36 -0500

    update navigation

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
 import Navigation from './components/navigation'
-import Header from './components/header'
-import Main from './components/main';
-import Footer from './components/footer';
 
 function App() {
   return (
-    <div className="container">
+    <>
     <Navigation/>
-    <Header/>
-    <Main />
-    <Footer/>
-    </div>
+    </>
   );
 }
 
--- myapp/src/components/navigation.jsx
+++ myapp/src/components/navigation.jsx
@@ -10,26 +10,55 @@ import About from '../pages/about';
 
 function Navigation() {
 
-	const [navigationScreen, setNavigationScreen] = useState(false);
+function showSideBar() {
+	const sidebar = document.querySelector('.sidebar')
+	sidebar.style.display = 'flex';
+}
 
-	const toggleNav = () => {
-        setNavigationScreen(!navigationScreen);
-    }
+function hideSideBar() {
+	const sidebar = document.querySelector('.sidebar')
+	sidebar.style.display = 'none';
+}
 
 	return (
 		<Router>
-		<nav className="navbar-container">
-			<Link to="/" className="navbar-logo">
-				<img src={Logo} alt="Logo" />
-			</Link>
-
-			<div className="burger-menu" onClick={toggleNav}>
-                    <div></div>
-                    <div></div>
-                    <div></div>
-                </div>
-
-			<ul className={`navbar-items ${navigationScreen ? 'nav-true' : ''}`}>
+		<nav>
+			<ul>
+				<li>
+					<Link to="/" className="anchor-logo">
+					<img src={Logo} alt="logo" />
+					</Link>
+				</li>
+				<li>
+					<Link className="hideOnMobile" to="/Home">Homepage</Link>
+				</li>
+				<li>
+					<Link className="hideOnMobile" to="/About">About</Link>
+				</li>
+				<li>
+					<Link className="hideOnMobile" to="/Menu">Menu</Link>
+				</li>
+				<li>
+					<Link className="hideOnMobile" o="/Reservations">Reservations</Link>
+				</li>
+				<li>
+					<Link className="hideOnMobile" to="/Order">Order</Link>
+				</li>
+				<li>
+					<Link className="hideOnMobile" to="/Login">Login</Link>
+				</li>
+				<li>
+				<Link className="menuButton" onClick={showSideBar}>
+				<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>
+				</Link>
+				</li>
+			</ul>
+			<ul className="sidebar">
+				<li>
+				<Link onClick={hideSideBar}>
+					<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg>
+				</Link>
+				</li>
 				<li>
 					<Link to="/Home">Homepage</Link>
 				</li>
