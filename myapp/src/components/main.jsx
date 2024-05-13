e6f0f36132fb5f0e44f067ef1bef666088b939db
Mark-J-H <markjameshinton@gmail.com>
2024-05-12 20:48:53 -0400

    more changes

--- myapp/src/components/main.jsx
+++ myapp/src/components/main.jsx
@@ -72,57 +72,74 @@ function Testimonials() {
 	return (
 		<div className="all-testimonials">
 			<div className="testimonials-title-and-button">
-				<h1>Testimonials</h1>
+				<h1>What Our Customers Say</h1>
 			</div>
 			<div className="testimonials-cards">
-				<div className="testimonials-card1">
+				<div className="testimonials-card">
 					<div className="testimonials-rating">
 						<img className="star-img" src={Star} alt="star" />
 						<img className="star-img" src={Star} alt="star" />
 						<img className="star-img" src={Star} alt="star" />
 					</div>
-					<img src={TestimonialImage1} alt="testimonial" />
-					<p className="testimonials-card-description">
-						I came with my friends for my birthday and everyone had a great
-						time!
-					</p>
+					<div className="testimonials-img-and-text-wrapper">
+					<div className="testimonials-img-and-text">
+						<img src={TestimonialImage1} alt="testimonial" />
+						<p className="testimonials-card-description">
+							I came with my friends for my birthday and everyone had a great
+							time!
+						</p>
+					</div>
+					</div>
 				</div>
-				<div className="testimonials-card2">
+				<div className="testimonials-card">
 					<div className="testimonials-rating">
 						<img className="star-img" src={Star} alt="star" />
 						<img className="star-img" src={Star} alt="star" />
 						<img className="star-img" src={Star} alt="star" />
 					</div>
-					<img src={TestimonialImage2} alt="testimonial" />
-					<p className="testimonials-card-description">
-						Great Food! Great People! Love it! Would highly recommend!
-					</p>
+					<div className="testimonials-img-and-text-wrapper">
+					<div className="testimonials-img-and-text">
+						<img src={TestimonialImage2} alt="testimonial" />
+						<p className="testimonials-card-description">
+							Great Food! Great People! Love it! Would highly recommend!
+						</p>
+					</div>
+					</div>
 				</div>
-				<div className="testimonials-card3">
+				<div className="testimonials-card">
 					<div className="testimonials-rating">
 						<img className="star-img" src={Star} alt="star" />
 						<img className="star-img" src={Star} alt="star" />
 						<img className="star-img" src={Star} alt="star" />
 						<img className="star-img" src={Star} alt="star" />
 					</div>
-					<img src={TestimonialImage3} alt="testimonial" />
-					<p className="testimonials-card-description">
-						Very reasonably priced for the service. I would recommend to
-						everyone.
-					</p>
+
+					<div className="testimonials-img-and-text-wrapper">
+					<div className="testimonials-img-and-text">
+						<img src={TestimonialImage3} alt="testimonial" />
+						<p className="testimonials-card-description">
+							Very reasonably priced for the service. I would recommend to
+							everyone.
+						</p>
+					</div>
+					</div>
 				</div>
-				<div className="testimonials-card3">
+				<div className="testimonials-card">
 					<div className="testimonials-rating">
 						<img className="star-img" src={Star} alt="star" />
 						<img className="star-img" src={Star} alt="star" />
 						<img className="star-img" src={Star} alt="star" />
 						<img className="star-img" src={Star} alt="star" />
 					</div>
-					<img src={TestimonialImage4} alt="testimonial" />
-					<p className="testimonials-card-description">
-						Very reasonably priced for the service. I would recommend to
-						everyone.
-					</p>
+					<div className="testimonials-img-and-text-wrapper">
+					<div className="testimonials-img-and-text">
+						<img src={TestimonialImage4} alt="testimonial" />
+						<p className="testimonials-card-description">
+							Very reasonably priced for the service. I would recommend to
+							everyone.
+						</p>
+					</div>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -136,7 +153,9 @@ function About() {
 				<div className="about-left-content">
 					<div className="titles">
 						<h1>Little Lemon</h1>
-						<h2>Chicago</h2>
+                        <svg className="squiggly-line" xmlns="http://www.w3.org/2000/svg" width="120" height="20" viewBox="0 0 120 20">
+                            <path d="M0,10 Q30,-10 60,10 T120,10" fill="none" stroke="#FFD700" strokeWidth="3"/>
+                        </svg>
 					</div>
 					<p>
 						Nestled in the heart of our community, our family-owned
